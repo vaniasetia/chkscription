c997b5e2b68b85a70a7940a8ca52df348a2f6730
vaniasetia <vanias2304@gmail.com>
2024-07-09 01:47:03 +0530

    Fix Medicines List

--- backend/server.py
+++ backend/server.py
@@ -21,15 +21,16 @@ def issue_prescription():
     data = request.get_json()
     patient_name = data.get('name')
     patient_age = data.get('age')
-    medicine = data.get('medicine')
+    medicines = data.get('medicines', [])
     gender_choice = data.get('gender')
     patient_weight = data.get('weight')
     patient_allergies = data.get('allergies')
     prescription_number = generate_prescription_number()
 
-    digital_signature_str = DSGen.generate_signature(f"{prescription_number}|{patient_name}|{patient_age}|{medicine}|{gender_choice}|{patient_weight}|{patient_allergies}".encode('utf-8'))
+    medicines_str = '|'.join([f"{med['name']}:{med['dosage']}:{med['frequency']}" for med in medicines])
+    digital_signature_str = DSGen.generate_signature(f"{prescription_number}|{patient_name}|{patient_age}|{medicines_str}|{gender_choice}|{patient_weight}|{patient_allergies}".encode('utf-8'))
 
-    prescription = Prescription(prescription_number, patient_name, patient_age, gender_choice, patient_weight, patient_allergies, medicine, digital_signature_str)
+    prescription = Prescription(prescription_number, patient_name, patient_age, gender_choice, patient_weight, patient_allergies, medicines, digital_signature_str)
     prescription.save()
 
     return jsonify({'message': 'prescription issued successfully', 'prescription_number': prescription_number, 'digital_signature': digital_signature_str}), 200
@@ -46,8 +47,10 @@ def verify_prescription():
     prescription = Prescription.find_by_prescription_number(prescription_number)
     if not prescription:
         return jsonify({'message': 'prescription not found', 'is_valid': False}), 200
+    
+    medicine_str = '|'.join([f"{med['name']}:{med['dosage']}:{med['frequency']}" for med in prescription.medicine])
 
-    message = f"{prescription.prescription_number}|{prescription.patient_name}|{prescription.patient_age}|{prescription.medicine}|{prescription.gender_choice}|{prescription.patient_weight}|{prescription.patient_allergies}".encode('utf-8')
+    message = f"{prescription.prescription_number}|{prescription.patient_name}|{prescription.patient_age}|{medicine_str}|{prescription.gender_choice}|{prescription.patient_weight}|{prescription.patient_allergies}".encode('utf-8')
     is_valid = DSGen.verify_signature(message, digital_signature)
 
 
